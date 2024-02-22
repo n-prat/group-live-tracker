f750be6bed1eaa024c4dd554149ebf21c8fabed6
n-prat <1504071+n-prat@users.noreply.github.com>
2024-02-22 17:20:20 +0100

    github pages: TRY and fix blank page

--- prod.sh
+++ prod.sh
@@ -2,8 +2,9 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+# public-url: MUST ALSO update "base" in index.html ! Else blank page when deploying on GH pages!
 pushd frontend
 JWT_SECRET=123456789 CARGO_TARGET_DIR=../target-trunk trunk build --release --public-url "group-live-tracker/"
 popd
 
-# cargo run --bin server --release -- --port 8080 --static-dir ./docs
+# cargo run --bin server --release -- --port 8080 --static-dir ./dist
