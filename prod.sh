f2cc413bf21dbf7cb0cda7ff2616920302b35245
n-prat <1504071+n-prat@users.noreply.github.com>
2024-02-22 16:26:38 +0100

    publish with github pages

--- prod.sh
+++ prod.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 IFS=$'\n\t'
 
 pushd frontend
-CARGO_TARGET_DIR=../target-trunk trunk build --release --public-url /
+JWT_SECRET=123456789 CARGO_TARGET_DIR=../target-trunk trunk build --release --public-url "group-live-tracker/"
 popd
 
-cargo run --bin server --release -- --port 8080 --static-dir ./dist
+# cargo run --bin server --release -- --port 8080 --static-dir ./docs
